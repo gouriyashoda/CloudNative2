d0647d5ba8507f059597c51e69fc6f589b1f752b
Yashodha Gouri <126040562+gouriyashoda@users.noreply.github.com>
2023-12-23 20:46:55 +0530

    Update DemoApplication.java

--- DemoApplication.java
+++ DemoApplication.java
@@ -7,46 +7,42 @@ public class DemoApplication {
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 SpringApplication.run(DemoApplication.class, args);
-ApplicationContext ac = new ClassPathXmlApplicationContext(&quot;testBean.xml&quot;);
-Customer c = (Customer) ac.getBean(&quot;customer&quot;);
+ApplicationContext ac = new ClassPathXmlApplicationContext("testBean.xml");
+Customer c = (Customer) ac.getBean("customer");
 Ticket t = (Ticket) c.getTick();
 while(true) {
-System.out.println(&quot;1. insert the details\n 2. Display details \n&quot;
-+ &quot;3. exit&quot;);
-System.out.println(&quot;enter the choice&quot;);
+System.out.println("1. insert the details\n 2. Display details\n 3. exit");
+System.out.println("enter the choice");
 int a = sc.nextInt();
 switch(a) {
 case 1:
-System.out.println(&quot;enter the customer name&quot;);
+System.out.println("enter the customer name");
 
 c.setName(sc.next());
-System.out.println(&quot;enter the customer address&quot;);
+System.out.println("enter the customer address");
 c.setAddress(sc.next());
-System.out.println(&quot;enter the ticket number&quot;);
+System.out.println("enter the ticket number");
 t.setTicno(sc.nextInt());
-System.out.println(&quot;enter the ticket seat no&quot;);
+System.out.println("enter the ticket seat no");
 t.setSeatno(sc.nextInt());
-System.out.println(&quot;enter the ticket price&quot;);
+System.out.println("enter the ticket price");
 t.setPrice(sc.nextInt());
-System.out.println(&quot;enter the ticket Type -
-
-economic/gold/platinum&quot;);
+System.out.println("enter the ticket Type -economic/gold/platinum");
 
 t.setTicktype(sc.next());
 c.setTick(t);
-System.out.println(&quot;thanks for input&quot;);
+System.out.println("thanks for input");
 break;
 
 case 2:
-System.out.println(&quot;Customer Details:&quot;);
-System.out.println(&quot;Name:&quot;+c.getName()+&quot; &quot;+&quot;
-Address:&quot;+c.getAddress());
-System.out.println(&quot;Ticket Details&quot;);
+System.out.println("Customer Details:");
+System.out.println("Name:"+c.getName()+"Address:"+c.getAddress());
+System.out.println("Ticket Details");
 t = c.getTick();
-System.out.println(&quot;Ticket No: &quot;+t.getTicno());
-System.out.println(&quot;Ticket Type: &quot;+t.getTicktype());
-System.out.println(&quot;Ticket Seat No: &quot;+t.getSeatno());
-System.out.println(&quot;Ticket Price: &quot;+t.getPrice());
+System.out.println("Ticket No: "+t.getTicno());
+System.out.println("Ticket Type: "+t.getTicktype());
+System.out.println("Ticket Seat No: "+t.getSeatno());
+System.out.println("Ticket Price: "+t.getPrice());
 break;
 case 3:
 System.exit(0);
