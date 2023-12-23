94cdafd52b74914a38375f820f4325c22409e824
Yashodha Gouri <126040562+gouriyashoda@users.noreply.github.com>
2023-12-23 20:58:35 +0530

    Update DemoApplication.java

--- DemoApplication.java
+++ DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Scanner;
-//imports
+
 
 @SpringBootApplication
 public class DemoApplication {
