b37895eeca73374a5bde0afb294d4f659719c296
Yashodha Gouri <126040562+gouriyashoda@users.noreply.github.com>
2023-12-23 20:57:52 +0530

    Update DemoApplication.java

--- DemoApplication.java
+++ DemoApplication.java
@@ -1,5 +1,8 @@
 Demo or Main Class:
 package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
 //imports
 
 @SpringBootApplication
